59342a3a176ee5583681c558b3eb86a8b5d8562f
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-02-22 15:03:42 -0600

    Added project0.sln skeleton and .gitignore

---
+++ Project0/Project0.Library/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address address { get; set; }
+        public Store defaultStore { get; set; } = new Store();
+
+        //Cannot place more than one order from the same location within two hours?
+
+        //Constructors
+        public Customer(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public Customer(string first, string last, Store newDefaultStore)
+        {
+            FirstName = first;
+            LastName = last;
+            defaultStore = newDefaultStore;
+        }
+
+
+
+    }
+}
